222d9fd4d726e680a71cb29b19e73e883d3cb796
Miniieee <106557722+Miniieee@users.noreply.github.com>
2025-02-15 17:59:39 +0000

    asd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,40 +1,12 @@
-using System;
-using UnityEngine;
 using Unity.Cinemachine;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-     //private GameManager gameManager;
+    //private GameManager gameManager;
     private CinemachineInputAxisController inputAxisController;
     private PlayerMovement playerMovement;
-    private bool hasReachedFinish;
-    public bool HasReachedFinish {get => hasReachedFinish; set => hasReachedFinish = value;}
-    
-    void Start()
-    {
-        //gameManager = FindFirstObjectByType<GameManager>();
-        
-    }
-
-    private void HandleGameStateChanged(LightState state)
-    {
-        if (state is LightState.GameOver or LightState.Won)
-        {
-            inputAxisController = GetComponentInChildren<CinemachineInputAxisController>();
-            playerMovement = GetComponent<PlayerMovement>();
-            inputAxisController.enabled = false;
-            playerMovement.enabled = false;
-        }
-    }
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("FinishLine"))
-        {
-            hasReachedFinish = true;
-        }
-    }
+    public bool HasReachedFinish { get; set; }
 
     private void OnEnable()
     {
@@ -46,4 +18,21 @@ public class Player : MonoBehaviour
         GameManager.OnGameStateChanged -= HandleGameStateChanged;
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("FinishLine")) HasReachedFinish = true;
+    }
+
+
+    private void HandleGameStateChanged(LightState state)
+    {
+        if (state is LightState.GameOver or LightState.Won)
+        {
+            inputAxisController = GetComponentInChildren<CinemachineInputAxisController>();
+            playerMovement = GetComponent<PlayerMovement>();
+            inputAxisController.enabled = false;
+            playerMovement.enabled = false;
+        }
+    }
 }
